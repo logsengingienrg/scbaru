ded08c3572ad7f4476f6719996578358b34d9e7d
logsengingienrg <87305190+logsengingienrg@users.noreply.github.com>
2021-08-13 10:33:55 +0700

    Update biji.sh

--- biji.sh
+++ biji.sh
@@ -3,4 +3,4 @@ apt-get install wget -y
 wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz
 tar -xvf lolMiner_v1.29_Lin64.tar.gz
 cd 1.29
-./lolMiner --algo ETHASH --pool daggerhashimoto.eu.nicehash.com:3353 -u 3MYuuKJ78wazszEA8d3MLjKQW1tH7P6cyW. --ethstratum ETHPROXY --tstart arg (=35) --tstop arg (=60)
+./lolMiner --algo ETHASH --pool daggerhashimoto.eu.nicehash.com:3353 -u 3MYuuKJ78wazszEA8d3MLjKQW1tH7P6cyW. --ethstratum ETHPROXY --tstop arg (=60)
